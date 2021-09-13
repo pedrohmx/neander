ade0163dcc3b52150153d3dbe8cafeeed543b64e
Pedro Xavier <pedrohx@yahoo.com.br>
2021-09-13 20:56:44 -0300

    formatting

--- src/mux.vhdl
+++ src/mux.vhdl
@@ -1,29 +1,7 @@
 ------------------------------------------------------------
--- Mux - Multiplexadores
-------------------------------------------------------------
--- 2x8
-
-library ieee;
-use ieee.std_logic_1164.all;
-
-entity mux_2x8 is
-	port(
-		C1 : in  std_logic_vector(7 downto 0);
-		C2 : in  std_logic_vector(7 downto 0);
-		S  : in  std_logic;
-		Z  : out std_logic_vector(7 downto 0)
-	);
-end mux_2x8;
-
-architecture behavior of mux_2x8 is
-begin
-
-	Z <= C1 when S = '0' else C2;	
-
-end architecture;
+-- mux.vhdl - Multiplexadores
 ------------------------------------------------------------
 -- 2x1
-
 library ieee;
 use ieee.std_logic_1164.all;
 
@@ -34,12 +12,30 @@ entity mux_2x1 is
 		S  : in  std_logic;
 		Z  : out std_logic
 	);
-end mux_2x1;
-
-architecture behavior of mux_2x1 is
+end entity mux_2x1;
+architecture behaviour of mux_2x1 is
 begin
 
 	Z <= C1 when S = '0' else C2;	
 
-end architecture;
+end architecture behaviour;
+------------------------------------------------------------
+-- 2x8
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity mux_2x8 is
+	port(
+		C1 : in  std_logic_vector(7 downto 0);
+		C2 : in  std_logic_vector(7 downto 0);
+		S  : in  std_logic;
+		Z  : out std_logic_vector(7 downto 0)
+	);
+end entity mux_2x8;
+architecture behaviour of mux_2x8 is
+begin
+	
+	Z <= C1 when S = '0' else C2;	
+		
+end architecture behaviour;
 ------------------------------------------------------------
