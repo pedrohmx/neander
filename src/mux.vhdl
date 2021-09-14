ad9b61ce1005e879a5a3badd499f4fa8a6ac7d20
Pedro Xavier <pedrohx@yahoo.com.br>
2021-09-14 12:57:41 -0300

    changed port names to be more consistent

--- src/mux.vhdl
+++ src/mux.vhdl
@@ -7,17 +7,15 @@ use ieee.std_logic_1164.all;
 
 entity mux_2x1 is
 	port(
+		C0 : in  std_logic;
 		C1 : in  std_logic;
-		C2 : in  std_logic;
 		S  : in  std_logic;
 		Z  : out std_logic
 	);
 end entity mux_2x1;
 architecture behaviour of mux_2x1 is
 begin
-
-	Z <= C1 when S = '0' else C2;	
-
+	Z <= C0 when S = '0' else C1;
 end architecture behaviour;
 ------------------------------------------------------------
 -- 2x8
@@ -26,16 +24,14 @@ use ieee.std_logic_1164.all;
 
 entity mux_2x8 is
 	port(
+		C0 : in  std_logic_vector(7 downto 0);
 		C1 : in  std_logic_vector(7 downto 0);
-		C2 : in  std_logic_vector(7 downto 0);
 		S  : in  std_logic;
 		Z  : out std_logic_vector(7 downto 0)
 	);
 end entity mux_2x8;
 architecture behaviour of mux_2x8 is
 begin
-	
-	Z <= C1 when S = '0' else C2;	
-		
+	Z <= C0 when S = '0' else C1;
 end architecture behaviour;
 ------------------------------------------------------------
