eac3b92df25907cb0f182ab60bf763d330bbdc76
Pedro Xavier <pedrohx@yahoo.com.br>
2021-09-17 01:07:32 -0300

    Current progress

---
+++ src/neander_aio.vhdl
@@ -0,0 +1,106 @@
+------------------------------------------------------------
+--	Neander
+------------------------------------------------------------
+--	Componentes
+--		Modulo ULA
+--		Modulo Memoria
+--		Modulo Controle
+------------------------------------------------------------
+--  Neander
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+
+entity neander is
+	port (
+		cl, clk : in std_logic
+		);
+end entity neander;
+architecture behaviour of neander is
+	component modulo_ULA is
+		port (
+			barramento : inout std_logic_vector(7 downto 0);
+			ula_op : in std_logic_vector(2 downto 0);
+			ac_rw : in std_logic;
+			mem_rw : in std_logic;
+			clk : in std_logic;
+			cl : in std_logic;
+			flags : out std_logic_vector(1 downto 0)
+		);
+	end component;
+	component modulo_MEM is
+		port (
+			barramento : inout std_logic_vector(7 downto 0);
+			addr_barr : in std_logic_vector(7 downto 0);
+			addr_pc : in std_logic_vector(7 downto 0);
+			barrPC : in std_logic;
+			rem_rw : in std_logic;
+			mem_rw : in std_logic;
+			rdm_rw : in std_logic;
+			clk : in std_logic;
+			cl : in std_logic--;
+		);
+	end component;
+	component RIP is
+		port (
+			barramento : in std_logic_vector(7 downto 0);
+			barr_inc : in std_logic;
+			pc_rw : in std_logic;
+			clk, cl : in std_logic;
+			addr_out : in std_logic_vector(7 downto 0)
+		);
+	end component;
+	component CTRL is
+		port (
+			barr_di : in std_logic_vector(7 downto 0);
+			nz : in std_logic_vector(1 downto 0);
+			barr_crtl : out std_logic_vector(10 downto 0)
+		);
+	end component;
+	signal s_barr_di : std_logic_vector(7 downto 0);
+	signal s_pc2mem : std_logic_vector(7 downto 0);
+	signal s_nz : std_logic_vector(1 downto 0);
+	--signal barr_crtl : std_logic_vector(11 downto 0);
+	signal s_barr_inc : std_logic;
+	signal s_barr_pc : std_logic;
+	signal s_ula_op : std_logic_vector(2 downto 0);
+	signal s_pc_rw : std_logic;
+	signal s_ac_rw : std_logic;
+	signal s_mem_rw : std_logic;
+	signal s_rem_rw : std_logic;
+	signal s_rdm_rw : std_logic;
+	signal s_ri_rw : std_logic;
+begin
+	u_m_ula : modulo_ULA port map(
+		s_barr_di,
+		s_ula_op, s_ac_rw, s_mem_rw,
+		clk, cl,
+		s_nz
+	);
+	u_m_mem : modulo_MEM port map(
+		-- barramento e end_barr recebem o mesmo sinal
+		-- porem, o barramento é inout apenas na interface de barr
+		s_barr_di, s_barr_di, s_pc2mem,
+		s_barr_pc, s_rem_rw, s_mem_rw, s_rdm_rw,
+		clk, cl
+	);
+	u_m_rip : RIP port map(
+		s_barr_di, s_barr_inc, s_pc_rw, clk, cl, s_pc2mem
+	);
+	u_m_ctrl : CTRL port map(
+		barr_di => s_barr_di,
+		nz => s_nz,
+		-- desmontndo barramento de controle em varios sinais
+		barr_crtl(10) => s_barr_inc,
+		barr_crtl(9) => s_barr_pc,
+		barr_crtl(8 downto 6) => s_ula_op,
+		barr_crtl(5) => s_pc_rw,
+		barr_crtl(4) => s_ac_rw,
+		barr_crtl(3) => s_mem_rw,
+		barr_crtl(2) => s_rem_rw,
+		barr_crtl(1) => s_rdm_rw,
+		barr_crtl(0) => s_ri_rw
+	);
+end architecture behaviour;
+
+------------------------------------------------------------
--- src/neander_ctrl.vhdl
+++ src/neander_ctrl.vhdl
@@ -1,3 +1,115 @@
 ------------------------------------------------------------
--- Neander - Modulo de Controle
+--	Neander - Modulo de Controle
+------------------------------------------------------------
+--	Componentes
+--		PC (RIP - Registrador de ponteiro de funcao)
+--		RI (IR - Registrador de instrução)
+--		Decodificador (Decode)
+------------------------------------------------------------
+--	RIP
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+
+entity RIP is
+	port (
+		barramento : in std_logic_vector(7 downto 0);
+		--
+		barr_inc : in std_logic;
+		pc_rw : in std_logic;
+		--
+		clk, cl : in std_logic;
+		--
+		addr_out : out std_logic_vector(7 downto 0)
+	);
+end entity RIP;
+architecture behaviour of RIP is
+	component fAdder8b is
+		port (
+			A, B : in  std_logic_vector(7 downto 0);
+			Cin  : in  std_logic;
+			Cout : out std_logic;
+			S    : out std_logic_vector(7 downto 0)
+		);
+	end component;
+	component regc8b is
+		port (
+			d      : in  std_logic_vector(7 downto 0);
+			rw     : in  std_logic;
+			clk    : in  std_logic;
+			pr, cl : in  std_logic;
+			s      : out std_logic_vector(7 downto 0)
+		);
+	end component;
+	signal sadd, s_mux2pc, s_PCatual : std_logic_vector(7 downto 0);
+	signal cout : std_logic;
+begin
+	u_fa8b : fAdder8b port map(
+		"00000001", s_PCatual, '0', cout, sadd
+	);
+	-- mux
+	s_mux2pc <= barramento when barr_inc = '0' else sadd;
+	u_reg_pc : regc8b port map(
+		s_mux2pc, pc_rw, clk, '1', cl, s_PCatual
+	);
+	addr_out <= s_PCatual;
+end architecture behaviour;
+------------------------------------------------------------
+--	Decoder
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+
+entity decoder is
+	port (
+		instr_in : in std_logic_vector(7 downto 0);
+		instr_out : out std_logic_vector(10 downto 0)
+	);
+end entity decoder;
+architecture behaviour of decoder is
+begin
+	-- big wall of 0s
+	with instr_in(7 downto 4) select instr_out <=
+		"10000000000" when "0000",--NOP
+		"01000000000" when "0001",--STA
+		"00100000000" when "0010",--LDS
+		"00010000000" when "0011",--ADD
+		"00001000000" when "0100",--OR
+		"00000100000" when "0101",--AND
+		"00000010000" when "0110",--NOT
+		"00000001000" when "1000",--JMP
+		"00000000100" when "1001",--JN
+		"00000000010" when "1010",--JZ
+		"00000000001" when "1111",--HLT
+		"00000000000" when others;
+end architecture behaviour;
+------------------------------------------------------------
+--	UC
+------------------------------------------------------------
+--	Modulo de controle
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+
+entity CTRL is
+	port (
+		barr_di : in std_logic_vector(7 downto 0);
+		nz : in std_logic_vector(1 downto 0);
+		barr_crtl : out std_logic_vector(10 downto 0)
+	);
+end entity CTRL;
+architecture behaviour of CTRL is
+	component regc8b is
+		port (
+			d      : in  std_logic_vector(7 downto 0);
+			rw     : in  std_logic;
+			clk    : in  std_logic;
+			pr, cl : in  std_logic;
+			s      : out std_logic_vector(7 downto 0)
+		);
+	end component;
+	--signal s_decoded 
+begin
+	
+end architecture behaviour;
 ------------------------------------------------------------
